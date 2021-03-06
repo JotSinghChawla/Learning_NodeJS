5e9c0db8132003c738785405183f208e2e1be268
Jot singh <jot.chawla.54.js@gmail.com>
2021-03-07 00:21:28 +0530

    GET static html file using HTTP server

--- node-http/index.js
+++ node-http/index.js
@@ -1,15 +1,47 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer( (req,res) => {
-    console.log(req.headers);
+    console.log( 'Request for ' + req.url + ' by method ' + req.method );
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>HELLO WORLD USING NODEJS</h1></body></html>');
+    if( req.method == 'GET') {
+        var fileUrl;
+        if( req.url == '/') fileUrl = '/index.html'
+        else fileUrl = req.url;
 
+        var filePath = path.resolve('./public'+fileUrl);
+        const fileExt = path.extname(filePath);
+        if( fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if( !exists ) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/plain');
+                    res.end('Error 404: ' + fileUrl + ' at ' + filePath + ' File not Found');
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            })
+
+        }
+        else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Error 404: ' + fileUrl + ' at ' + filePath + ' File is not an HTML File');
+            return;
+        }
+    }
+    else {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Error 404: ' + req.method + ' is not supported');
+        return;
+    }
 })
 
 server.listen(port, hostname, () => {
