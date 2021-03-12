d9c847a493f5fce1dc64a4d64204364e7cf41d34
Jot singh <jot.chawla.54.js@gmail.com>
2021-03-12 22:18:50 +0530

    Update dishRouter.js

--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -29,4 +29,33 @@ dishRouter.route('/')
     res.end('Deleting all the Dishes!!!');
 });             // Semicolon here is necessary & We have done the chaining of REST verbs up above
 
+
+
+dishRouter.route('/:dishId')
+
+.all( (req,res,next) => {
+    res.statusMessage = 200;
+    res.setHeader('Content-Type','text/plain');
+    next();
+})
+
+.get( (req,res) => {
+    res.end('THis is a Dishes Database static with Specific Dish: ' + req.params.dishId);
+}) 
+
+.post( (req,res) => {
+    res.statusCode = 403;
+    res.end('POST operation is not supported on /dishes/' + req.params.dishId );
+})
+
+.put( (req,res) => {
+    res.write('Updating the dish: ' + req.params.dishId );
+    res.end('\nWill UPDATE your single dish request in dishes: '+ req.body.name + ' => ' +  req.body.description );
+})
+
+.delete( (req,res) => {
+    res.end('Deleting the Single Dish! with id = ' + req.params.dishId );
+}); 
+
+
 module.exports = dishRouter;
