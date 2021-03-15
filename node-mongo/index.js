4c41d61d0fcc7d78ee0a2707a365bc64e91f37d4
Jot singh <jot.chawla.54.js@gmail.com>
2021-03-15 23:17:22 +0530

    Used Promises to solve Callback Hell

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,40 +5,53 @@ const dbOperations = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err, client) => {
+MongoClient.connect(url).then( (client) => {
     
-    assert.equal(err, null);
+    // assert.strictEqual(err, null);                     // No need to check error with assert
 
     console.log('Connect correctly to the Server');
 
     const db = client.db(dbname);
 
-    dbOperations.insertDocument(db, { name: "Vadonut", description: "Test Vadonut"}, 'dishes', (result) => {
+    dbOperations.insertDocument(db, { name: "Vadonut", description: "Test Vadonut"}, 'dishes')
+        .then( (result) => {
     
-        console.log('Insert Document: \n', result.ops);     // ops Tells us the number of insert operations 
+            console.log('Insert Document: \n', result.ops);     // ops Tells us the number of insert operations 
+        
+            return dbOperations.findDocuments(db, 'dishes');
+        })
 
-        dbOperations.findDocuments(db, 'dishes', (docs) => {
+        .then( (docs) => {
 
             console.log('Found Documents: \n', docs);
 
-            dbOperations.updateDocument(db, {name: 'Vadonut'}, { description: 'Updated Test'}, 'dishes', (result) => {
+            return dbOperations.updateDocument(db, {name: 'Vadonut'}, { description: 'Updated Test'}, 'dishes');
+        })
+        
+        .then( (result) => {
 
-                console.log('Updated Document: ', result.result);
+            console.log('Updated Document: ', result.result);
 
-                dbOperations.findDocuments(db, 'dishes', (docs) => {
+            return dbOperations.findDocuments(db, 'dishes');
+        })
+      
+        .then( (docs) => {
+    
+            console.log('Found Updated Documents: \n', docs);
+
+            return db.dropCollection('dishes');
+        })
+        
+        .then( (result) => {
+
+            console.log('Dropped Collection: ', result);
             
-                    console.log('Found Updated Documents: \n', docs);
-
-                    db.dropCollection('dishes', (result) => {
-                        console.log('Dropped Collection: ', result);
-                    });
-                }); 
-            });
+            client.close();
+        })
+        
+        .catch( (err) => {
+            console.log(err)
         });
-    });
-
-
-
 
 
     // const collection = db.collection('dishes');
@@ -61,4 +74,7 @@ MongoClient.connect(url, (err, client) => {
     //     });
     // });
 
+})
+.catch( (err) => {
+    console.log(err)
 });
