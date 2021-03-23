a15159b2f2cb7b8fd8b3a5dc0e97c13baa7ac8e0
Jot singh <jot.chawla.54.js@gmail.com>
2021-03-24 01:20:36 +0530

    Excluded Session & Auth to Work with JWT Authentication in separate Routes & METHODS

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -7,6 +7,7 @@ var session = require('express-session');
 var Filestore = require('session-file-store')(session);                 // (session)   ???
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/userRouter');
@@ -15,10 +16,12 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
+// mongoose.Promise = require('bluebird');                          // IDK why it is used
 
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+// const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then( (db) => {
@@ -36,70 +39,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-01234-54321'));               // Random Signed Cookie
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-01234-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new Filestore()
-}));
+// app.use(session({                            // As we are using JWT instead
+//   name: 'session-id',
+//   secret: '12345-67890-01234-54321',
+//   saveUninitialized: false,
+//   resave: false,
+//   store: new Filestore()
+// }));
 
 app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.session());                  // As we are using JWT instead
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth( req, res, next ) {
-  console.log(req.headers , "  \nCookies: " , req.signedCookies, "  \nSession: " , req.session, );
-
-  // if (!req.signedCookies.user) {
-  // if ( !req.session.user) {
-    if ( !req.user ) {                               // After implementing Passport-local 
-    // var authHeader = req.headers.authorization; 
-    // if (!authHeader) {
-      var err = new Error('You are not Authenticated!');
-      // res.setHeader('WWW-Authenticate', 'Basic'); 
-      err.status = 403;
-      return next(err);
-       
-  //   var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-  //   var username = auth[0];
-  //   var password = auth[1];
-
-  //   if (username === 'admin' && password === 'password') {
-  //     // res.cookie('user', 'superuser', { signed: true });
-  //     req.session.user = 'superuser';
-  //     next();                                                       // Will Pass the request to Next Middleware
-  //   }
-  //   else {
-  //     var err = new Error('Invalid Username/Password');
-
-  //     res.setHeader('WWW-Authenticate', 'Basic');
-  //     err.status = 401;
-  //     return next(err);
-  //   }
-  }
-  else {
-    // if (req.signedCookies.user == 'superuser') {
-    next();
-    // if (req.session.user === 'user_authenticated') {               // After Passport-local
-    //   next();
-    // }
-    // else {
-    //   var err = new Error('You are not authenticated:  Invalid Cookie');
-    //   // res.setHeader('WWW-Authenticate', 'Basic');
-    //   err.status = 403;
-    //   return next(err);
-    // }                                                              // After Passport-local
-  }
-
-}
-
-app.use(auth);      // Function
-
 app.use(express.static(path.join(__dirname, 'public')));
+
+// The User Verification is done in very Route JS file  using the method .verifyUser() from ./authenticate.js 
 
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
