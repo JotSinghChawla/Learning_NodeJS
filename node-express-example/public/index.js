dc7a04d6666ebf936d6ca80b0160bebf20573879
Jot singh <jot.chawla.54.js@gmail.com>
2021-03-23 16:49:50 +0530

    Few Changes

--- node-express-example/public/index.js
+++ node-express-example/public/index.js
@@ -20,7 +20,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         let addRow = "";
         for( let j=0; j<4; j++) {
             if(  j + i >= product.length ) break;
-            console.log('yes');
             addRow += `<div class="col-12 col-md-3"><div class="card" style="max-width: 18rem;"> <img class="card-img-top img-fluid rounded" style="min-height: 45vh; background-position: center" src=${product[j+i].image} > <div class="card-body"> <h5 class="card-title">${product[j+i].name}</h5> <p class="card-text">Price: ${product[j+i].price}</p> <a href="#" class="btn btn-primary">Add Item</a> <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modal${j+i}">Description</button> </div> </div></div> <div class="modal fade" id="modal${j+i}" > <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <h5 class="modal-title" id="exampleModalLabel">Description: ${product[j+i].name}</h5> <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button> </div> <div class="modal-body"> ${product[j+i].description} </div> <div class="modal-footer"> <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button> </div> </div> </div> </div>`;
         }
         newRow.innerHTML = addRow;
