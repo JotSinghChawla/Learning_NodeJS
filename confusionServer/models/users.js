18053f6e7aa61e4c113a37dc74fad1b637f14ffd
Jot singh <jot.chawla.54.js@gmail.com>
2021-03-23 18:06:51 +0530

    Changed User Schema & connect Passport-local

--- confusionServer/models/users.js
+++ confusionServer/models/users.js
@@ -1,21 +1,25 @@
 var mongoose = require('mongoose');
-const { use } = require('../routes');
+// const { use } = require('../routes');
 var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
 
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true    
-    },
-    password: {
-        type: String,
-        required: true 
-    },
+    // username: {                              // Will added by the Passport-Local-Mongoose 
+    //     type: String,
+    //     required: true,
+    //     unique: true    
+    // },
+    // password: {
+    //     type: String,
+    //     required: true 
+    // },
     admin: {
         type: Boolean,
         default: false
     }
 })
+
+User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
