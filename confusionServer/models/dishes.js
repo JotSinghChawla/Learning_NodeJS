047b5d5c65b13abd2d9d54e593e676c428280419
Jot singh <jot.chawla.54.js@gmail.com>
2021-03-25 13:34:36 +0530

    Used Mongoose Populate & changed author Schema type to ObjectID

--- confusionServer/models/dishes.js
+++ confusionServer/models/dishes.js
@@ -15,8 +15,8 @@ const commentSchema = new Schema({
         require: true
     },
     author: {
-        type: String,
-        required: true
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 
 },
